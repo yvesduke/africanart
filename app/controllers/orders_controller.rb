0ad9614d82508a208e9ae24268dee092ecf2848e
Yves D <yvesduke06@live.co.uk>
2014-08-21 00:24:32 +0100

    Add order history pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,24 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
+  end
+
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+  end
+
   # GET /orders
   # GET /orders.json
-  def index
-    @orders = Order.all
-  end
+  #def index
+   # @orders = Order.all
+  #end
 
   # GET /orders/1
   # GET /orders/1.json
-  def show
-  end
+  #def show
+  #end
 
   # GET /orders/new
   def new
@@ -20,8 +28,8 @@ class OrdersController < ApplicationController
   end
 
   # GET /orders/1/edit
-  def edit
-  end
+  #def edit
+  #end
 
   # POST /orders
   # POST /orders.json
@@ -47,27 +55,27 @@ class OrdersController < ApplicationController
 
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+    #respond_to do |format|
+      #if @order.update(order_params)
+        #format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+       # format.json { render :show, status: :ok, location: @order }
+      #else
+      #  format.html { render :edit }
+     #   format.json { render json: @order.errors, status: :unprocessable_entity }
+    #  end
+   # end
+  #end
 
   # DELETE /orders/1
   # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #def destroy
+    #@order.destroy
+    #respond_to do |format|
+     # format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+    #  format.json { head :no_content }
+   # end
+  #end
 
   private
     # Use callbacks to share common setup or constraints between actions.
