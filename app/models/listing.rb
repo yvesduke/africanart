cce71c7ba9a1b031454d14a481dd31d967a527be
Yves D <yvesduke06@live.co.uk>
2014-08-20 14:45:51 +0100

    Associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,7 +12,7 @@ validates :name, :description, :price, presence: true
 validates :price, numericality: { greater_than: 0 }
 validates_attachment_presence :image
 
-#belongs_to :user
+belongs_to :user
 #has_many :orders
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
    validates :name, presence: true
 
-   #has_many :listings, dependent: :destroy
+   has_many :listings, dependent: :destroy
    #has_many :sales, class_name: "Order", foreign_key: "saller_id"
    #has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
 end
