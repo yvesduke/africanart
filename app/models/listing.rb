7d19e35f8b7823781572436be5243f7643de48cb
Yves D <yvesduke06@live.co.uk>
2014-08-23 02:03:39 +0100

    Resolving Errors

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,6 +6,8 @@ class Listing < ActiveRecord::Base
                     :storage => :dropbox,
                     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                     :path => "style/:id_:filename"
+ 
+validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/ 
  end
 
 validates :name, :description, :price, presence: true
